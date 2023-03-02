e387e6d7c664574185304f28e42ccb69bdda827e
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-03-01 21:58:12 -0300

    Criando as funções necessárias para estimar os gestos

--- classes/class01/pages/titles/src/services/handGestureService.js
+++ classes/class01/pages/titles/src/services/handGestureService.js
@@ -1,12 +1,37 @@
+import { knownGestures, gestureStrings } from "../util/gestures.js"
+
 export default class HandGestureService {
-    #fingerpose
+    #gestureEstimator
     #handPoseDetection
     #handVersion
     #detector = null
     constructor({ fingerpose, handPoseDetection, handVersion }) {
-        this.#fingerpose = fingerpose
+        this.#gestureEstimator = new fingerpose.GestureEstimator(knownGestures)
         this.#handPoseDetection = handPoseDetection
         this.#handVersion = handVersion
+    }
+
+    async estimate(keypoints3D) {
+        const predictions = await this.#gestureEstimator.estimate(
+            this.#getLandMarksFromKeypoints(keypoints3D),
+            //porcentagem de confiança do gesto (90%)
+            9
+        )
+        console.log({predictions})
+    }
+
+    async * detectGestures(predictions) {
+        for (const hand of predictions) {
+            if(!hand.keypoints3D) continue
+
+            const gestures = await this.estimate(hand.keypoints3D)
+            console.log({gestures})
+        }
+    }
+
+    #getLandMarksFromKeypoints(keypoints3D) {
+        return keypoints3D.map(keypoint => 
+            [keypoint.x, keypoint.y, keypoint.z])
     }
 
     async estimateHands(video) {
